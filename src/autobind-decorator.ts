ef8a1d8df41a6c06f201166c7673ad1126daccac
Phaneendra Dulla <44269412+PhaneendraDulla@users.noreply.github.com>
2020-09-24 20:14:21 -0400

    Create autobind-decorator.ts

---
+++ src/autobind-decorator.ts
@@ -0,0 +1,14 @@
+// AutoBind Decorator
+namespace App {
+  export function autobind(_1: any, _2: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value;
+    const adjustedDescriptor: PropertyDescriptor = {
+      configurable: true,
+      get() {
+        const boundFn = originalMethod.bind(this);
+        return boundFn;
+      },
+    };
+    return adjustedDescriptor;
+  }
+}
