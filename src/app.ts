e5ddcab66bf81d27cf6fabac5ebdcbe90d8eb6fc
Phaneendra Dulla <44269412+PhaneendraDulla@users.noreply.github.com>
2020-09-24 20:14:49 -0400

    Imported Autobind Decorator

--- src/app.ts
+++ src/app.ts
@@ -2,21 +2,9 @@
 /// <reference path="project-model.ts" />
 /// <reference path="project-state.ts" />
 /// <reference path="validation.ts" />
+/// <reference path="autobind-decorator.ts" />
 
 namespace App {
-  // AutoBind Decorator
-  function autobind(_1: any, _2: string, descriptor: PropertyDescriptor) {
-    const originalMethod = descriptor.value;
-    const adjustedDescriptor: PropertyDescriptor = {
-      configurable: true,
-      get() {
-        const boundFn = originalMethod.bind(this);
-        return boundFn;
-      },
-    };
-    return adjustedDescriptor;
-  }
-
   // Component Base Class
   abstract class Component<T extends HTMLElement, U extends HTMLElement> {
     templateElement: HTMLTemplateElement;
