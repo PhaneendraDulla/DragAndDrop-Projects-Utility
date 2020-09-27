7a8fb0a2412aa9d5657a19d182db6bccab1c197d
Phaneendra Dulla <44269412+PhaneendraDulla@users.noreply.github.com>
2020-09-27 16:23:04 -0400

    Imported Project Item Component

--- src/app.ts
+++ src/app.ts
@@ -4,49 +4,9 @@
 /// <reference path="validation.ts" />
 /// <reference path="autobind-decorator.ts" />
 /// <reference path="base-component.ts" />
+/// <reference path="project-item-component.ts" />
 
 namespace App {
-  // Project Item Class
-  class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> implements Draggable {
-    private project: Project;
-
-    get Persons() {
-      if (this.project.people === 1) {
-        return "1 person";
-      } else {
-        return `${this.project.people} persons`;
-      }
-    }
-    constructor(hostId: string, project: Project) {
-      super("single-project", hostId, false, project.id);
-      this.project = project;
-
-      this.configure();
-      this.renderContent();
-    }
-
-    @autobind
-    dragStartHandler(event: DragEvent) {
-      event.dataTransfer!.setData("text/plain", this.project.id);
-      event.dataTransfer!.effectAllowed = "move";
-    }
-
-    dragEndHandler(_: DragEvent) {
-      console.log("DragEnd");
-    }
-
-    configure() {
-      this.element.addEventListener("dragstart", this.dragStartHandler);
-      this.element.addEventListener("dragend", this.dragEndHandler);
-    }
-
-    renderContent() {
-      this.element.querySelector("h2")!.textContent = this.project.title;
-      this.element.querySelector("h3")!.textContent = this.Persons + " assigned";
-      this.element.querySelector("p")!.textContent = this.project.description;
-    }
-  }
-
   // Project List Class
   class ProjectList extends Component<HTMLDivElement, HTMLElement> implements DragTarget {
     assignedProjects: Project[];
