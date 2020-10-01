5dc94740436a73899f3336a839e5f6888882fccb
Phaneendra Dulla <44269412+PhaneendraDulla@users.noreply.github.com>
2020-09-30 21:14:16 -0400

    Update project-list.ts

--- src/components/project-list.ts
+++ src/components/project-list.ts
@@ -1,3 +1,9 @@
+/// <reference path="base-component.ts" />
+/// <reference path="../decorators/autobind.ts" />
+/// <reference path="../models/project.ts" />
+/// <reference path="../models/drag-drop.ts" />
+/// <reference path="../state/project-state.ts" />
+
 // Project List Class
 namespace App {
   export class ProjectList extends Component<HTMLDivElement, HTMLElement> implements DragTarget {
